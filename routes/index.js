7186361dc08eb16468989297fa76c3bae4b7b24e
Amalkrishnanvp <Amalyedhu10@gmail.com>
2024-08-15 20:40:48 +0530

    Completed Project

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,27 @@ const express = require("express");
 const router = express.Router();
 
 /* GET Home Page */
-router.get("/", (req, res, next) => {
-  res.render("index", {
-    title: "Movie Search Application",
-    message: "Success",
-  });
+router.get("/", async (req, res, next) => {
+  // const apiKey = "b99f2eca";
+  const apiUrl = `http://www.omdbapi.com/?t=Vikram&apikey=b99f2eca`;
+
+  try {
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      throw new error("Movie api network response was not ok");
+    }
+    const data = await response.json();
+    console.log(data);
+
+    res.render("index", {
+      title: "Movie Search Application",
+      message: "Success",
+      movie: data,
+    });
+  } catch (error) {
+    console.error("There was a problem with fetch operation", error);
+  }
 });
 
 module.exports = router;
