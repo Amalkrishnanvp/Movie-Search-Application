81e2423fe52fc6520c85f51b43c490d25becee1e
Amalkrishnanvp <Amalyedhu10@gmail.com>
2024-08-16 13:14:15 +0530

    Added awards showing feature and fixed bugs

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -13,6 +13,20 @@ document.addEventListener("DOMContentLoaded", () => {
   const movieWriter = document.querySelector(".movie-writer");
   const movieRelease = document.querySelector(".movie-release");
   const movieBoxOffice = document.querySelector(".movie-box-office");
+  const movieType = document.querySelector(".movie-type");
+  const seriesTotalSeasons = document.querySelector(".series-total-seasons");
+  const seasonsContainer = document.querySelector(".seasons-container");
+  const movieAwards = document.querySelector(".movie-awards");
+
+  function makeUppercase() {
+    const movieTypeElement = document.querySelector(".movie-type");
+    const movieType = movieTypeElement.innerText;
+    const modifiedMovieType =
+      movieType.charAt(0).toUpperCase() + movieType.slice(1);
+    movieTypeElement.innerText = modifiedMovieType;
+  }
+
+  makeUppercase();
 
   async function searchMovie(movieName) {
     try {
@@ -49,12 +63,44 @@ document.addEventListener("DOMContentLoaded", () => {
     movieWriter.innerText = movieDetails.Writer;
     movieRelease.innerText = movieDetails.Released;
     movieBoxOffice.innerText = movieDetails.BoxOffice;
+    movieType.innerText = movieDetails.Type;
+    movieAwards.innerText = movieDetails.Awards;
+    makeUppercase();
+    episodeShower(movieDetails);
+  }
+
+  function episodeShower(movieDetails) {
+    if (movieDetails.Type === "movie") {
+      if (!seasonsContainer.classList.contains("hidden")) {
+        seasonsContainer.classList.add("hidden");
+      }
+    } else {
+      if (seasonsContainer.classList.contains("hidden")) {
+        seasonsContainer.classList.remove("hidden");
+        seriesTotalSeasons.innerText = movieDetails.totalSeasons;
+      } else {
+        seriesTotalSeasons.innerText = movieDetails.totalSeasons;
+      }
+    }
   }
 
   searchBtn.addEventListener("click", () => {
     if (cinemaHolder.value) {
       const movieName = cinemaHolder.value;
       searchMovie(movieName);
+    } else {
+      alert("Please enter a movie name before searching!");
+    }
+  });
+
+  cinemaHolder.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      if (cinemaHolder.value) {
+        const movieName = cinemaHolder.value;
+        searchMovie(movieName);
+      } else {
+        alert("Please enter a movie name before searching!");
+      }
     }
   });
 });
